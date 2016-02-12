c609fa8f5e2af0b5a82b156f135adaae67bc6de3
Jon Chan <me@jonhmchan.com>
2016-02-12 10:59:26 -0500

    Add scrolling interaction

--- index.js
+++ index.js
@@ -6,4 +6,16 @@
 		html += '</div>';
 		$(".main").append(html);
 	}
+
+	var lastScroll = $(window).scrollTop();
+
+	$(window).scroll(function() {
+		var scroll = $(this).scrollTop();
+		if (scroll > lastScroll) {
+			$("nav").slideUp();
+		} else {
+			$("nav").slideDown();
+		}
+		lastScroll = scroll;
+	})
 })(grid)
