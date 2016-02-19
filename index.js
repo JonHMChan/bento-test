4b256653966c9d87140b063d9b3fee96201ae39e
Innocent <innocent.amadi@andela.com>
2016-02-19 16:14:12 +0100

    modifications from last time

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 (function(grid) {
 	for (var key in grid) {
 		var topic = grid[key];
-		var html = '<div class="colored-box">';
-		html += topic["name"];
-		html += '</div>';
+		var html = '<div class="' + topic["tags"][0] + ' colored-box">';
+		html += '<h2><a class="title" href="/"' + topic["home"] + '"" title="Show full page">' + topic["name"] + '</a><h2>';
+		html += '<h3>' + topic["description"] + '</h3>'
+		// add loop through links array here
+		topic.links.forEach((link) => {
+			html += '<p><a href="' + link["url"] + '" alt="' + link["name"] + ' target="_blank">' + link["name"] + '</a></p>'
+		})
+		// end loop here
+		html += "</div>"
 		$(".main").append(html);
 	}
 
